d69d5eb09de3fe182e93ecdc916373e21f929175
Anuj Pal <palanuj247@gmail.com>
2024-11-05 21:45:18 +0530

    project setup done

--- src/app.js
+++ src/app.js
@@ -1,6 +1,27 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+// built in middleware
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+
+// third party middleware
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+
+// import routes
+
+
+// routes declaration
 
 
 export default app;
